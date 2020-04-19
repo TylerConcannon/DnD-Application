d000e5129b854b4fbcf74fdddc2bd5dcbf60a8ce
TylerConcannon <tzc6@psu.edu>
2020-04-18 21:56:42 -0400

    TownViewer kt and xml

--- T5Application/app/src/main/java/com/t5application/pages/dm/content/town/TownViewer.kt
+++ T5Application/app/src/main/java/com/t5application/pages/dm/content/town/TownViewer.kt
@@ -1,14 +1,25 @@
 package com.t5application.pages.dm.content.town
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 
 import com.t5application.R
 
 class TownViewer : Fragment() {
+
+    private lateinit var doneViewingTown: Button
+
+    private lateinit var nameTextView: TextView
+    private lateinit var sizeTextView: TextView
+    private lateinit var terrainTextView: TextView
+    private lateinit var buildingsTextView: TextView
+    private lateinit var politicsTextView: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,6 +29,19 @@ class TownViewer : Fragment() {
         val view =  inflater.inflate(R.layout.town_viewer, container, false)
 
         activity?.title = "Town"
+        nameTextView = view.findViewById(R.id.nameTextView)
+        sizeTextView = view.findViewById(R.id.sizeTextView)
+        terrainTextView = view.findViewById(R.id.terrainTextView)
+        buildingsTextView = view.findViewById(R.id.buildingTextView)
+        politicsTextView = view.findViewById(R.id.politicsTextView)
+
+        doneViewingTown = view.findViewById(R.id.doneTownButton)
+
+        activity?.title = "Town Viewer"
+
+        doneViewingTown.setOnClickListener {
+            view.findNavController().navigate(R.id.TownViewerToDMRecyclerView)
+        }
 
         return view;
     }
