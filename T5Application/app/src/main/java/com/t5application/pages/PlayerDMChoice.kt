a819ea9528561e5416872ebdba3d57a3d07ce178
Tyler Boothby <tylerjboothby@gmail.com>
2020-04-20 01:11:37 -0400

    Added one bomb ass media player
    FAR OVEEERRRRRR THE MISTY MOUNTAINS COOOOOLD

--- T5Application/app/src/main/java/com/t5application/pages/PlayerDMChoice.kt
+++ T5Application/app/src/main/java/com/t5application/pages/PlayerDMChoice.kt
@@ -1,5 +1,6 @@
 package com.t5application.pages
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,11 +9,15 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.findNavController
 import com.t5application.R
+import kotlinx.android.synthetic.main.player_dm_choice.*
 
 class PlayerDMChoice : Fragment() {
 
     private lateinit var PlayerButton: Button
     private lateinit var DMButton: Button
+    private lateinit var MusicButton: Button
+    private lateinit var mediaPlayer: MediaPlayer
+    private var playing: Boolean = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,6 +31,10 @@ class PlayerDMChoice : Fragment() {
 
         PlayerButton = view.findViewById(R.id.PlayerButton)
         DMButton = view.findViewById(R.id.DMButton)
+        MusicButton = view.findViewById(R.id.playMusicButton)
+        mediaPlayer = MediaPlayer.create(context, R.raw.sound_file)
+        mediaPlayer.isLooping = true
+        playing = false
 
         PlayerButton.setOnClickListener {
             view.findNavController().navigate(R.id.PlayerDMChoiceToPlayerRecyclerView)
@@ -33,6 +42,19 @@ class PlayerDMChoice : Fragment() {
 
         DMButton.setOnClickListener {
             view.findNavController().navigate(R.id.playerDMChoiceToSelectRecyclerView)
+        }
+
+        MusicButton.setOnClickListener{
+
+
+            mediaPlayer?.isPlaying
+            playing = if(!playing){
+                mediaPlayer?.start()
+                true
+            } else {
+                mediaPlayer?.pause()
+                false
+            }
         }
 
         return view
