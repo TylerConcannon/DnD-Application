13e5f7428614c0f8a80add856332704ac56e329e
TylerConcannon <tzc6@psu.edu>
2020-04-30 19:49:12 -0400

    Adventure Recycler View

---
+++ T5Application/app/src/main/java/com/t5application/pages/dm/recycler_views/AdventureRecyclerView.kt
@@ -0,0 +1,24 @@
+package com.t5application.pages.dm.recycler_views
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.t5application.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class AdventureRecyclerView : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.adventure_recycler_view, container, false)
+    }
+
+}
